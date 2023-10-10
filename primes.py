cb3c088cb6d2e7cf5627f5de3b831df48eb929d9
Zeenia Haji <zeenia.haji@kcl.ac.uk>
2023-10-10 12:51:29 +0100

    Add a function to list number of primes

--- primes.py
+++ primes.py
@@ -1,6 +1,28 @@
 """List of prime numbers generator."""
 """ENTER YOUR SOLUTION HERE!"""
-
 def primes(number_of_primes):
-    list = []
+
+    if number_of_primes <= 0:
+        raise ValueError
+    else:
+        list = []
+        i = 2
+        while len(list) < number_of_primes:
+            if i == 2: 
+               list.append(2)
+               
+            else:
+               flag = True
+               for number in range(2, i):
+                   if i % number == 0:
+                       flag = False     
+            
+               if flag == True:
+                    list.append(i)
+
+            i += 1
+        
     return list
+    #print(f'{list}') 
+        
+#print(primes(4))
